11555e58aee210d9adb25a7bd83498b27b280b00
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2025-06-19 20:17:46 +0530

    Configure Django to use PostgreSQL via DATABASE_URL and add dj-database-url

--- p2p_lender/settings.py
+++ p2p_lender/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +75,9 @@ WSGI_APPLICATION = "p2p_lender.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
